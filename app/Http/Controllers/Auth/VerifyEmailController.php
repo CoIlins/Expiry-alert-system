538bb0f4bdda0619e6cdecb240b50931001f6c2e
CoIlins <collinskinoti2006@gmail.com>
2026-06-24 12:48:18 +0300

    Redirect dashboard fixed

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -14,14 +14,23 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+    $user = $request->user();
+    $dashboardRoute = match ($user->role->role_name) {
+                'Admin'           => 'admin.dashboard',
+                'Vendor'          => 'vendor.dashboard',
+                'Inventory Clerk' => 'clerk.dashboard',
+                default           => 'dashboard',
+            };
+
+            if ($user->hasVerifiedEmail()) {
+                return redirect()->intended(route($dashboardRoute, absolute: false).'?verified=1');
+            }
+
+            if ($user->markEmailAsVerified()) {
+                event(new Verified($user));
+            }
+
+            return redirect()->intended(route($dashboardRoute, absolute: false).'?verified=1');
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
-    }
 }
